cec8964fefd1a9bd0f527e7d9abb95a75b917e5e
Pasuvula Ramesh <ramesh.pasuvula@artihcus.com>
2024-04-19 09:51:16 +0000

    Here single data struct added..

---
+++ webapp/controller/Home.controller.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, JSONModel) {
+        "use strict";
+
+        return Controller.extend("com.app.sapui5.controller.home", {
+            onInit: function () {
+                const oPerson = {
+                    employee: {
+                        EmployeeName: "John Doe",
+                        EmployeeAge: 40,
+                        ContractStarted: new Date(),
+                        Email: "john.doe@gmail.com"
+                    }
+                }
+                const oJsonModel = new JSONModel(oPerson);
+                this.getView().setModel(oJsonModel, "employeeModel");
+
+            },
+        });
+    });
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comapp/sapui5/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
