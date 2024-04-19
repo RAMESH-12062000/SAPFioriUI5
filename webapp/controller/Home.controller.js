845ff90c133ccd3a7068ca00d1e64aa3df63c610
Pasuvula Ramesh <ramesh.pasuvula@artihcus.com>
2024-04-19 12:14:28 +0000

    Table struct added..

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,12 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    //"sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    //here must should be used "JSONModel" parameter and which come from==>"sap/ui/model/json/JSONModel"
-    function (Controller, JSONModel) {
+    //here should be used for only one data binding "JSONModel" parameter and which come from==>"sap/ui/model/json/JSONModel"
+    function (Controller) {
         "use strict";
 
         return Controller.extend("com.app.sapfiori.controller.Home", {
